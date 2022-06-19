30885fb710095d34682725ee0119a02f51d223d0
burakduman-bilgeadam <burkdumn@gmail.com>
2022-06-19 23:27:36 +0300

    first

---
+++ src/main/java/com/homework/homeworkSpring/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.homework.homeworkSpring.controller;
+
+import com.homework.homeworkSpring.model.Product;
+
+import java.util.List;
+
+public class ProductController {
+    private static List<Product> productList;
+}
